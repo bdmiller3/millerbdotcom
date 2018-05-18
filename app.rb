0c9d2f7d295c6087c65b0376c841eed5febaf733
Benjamin Miller <bmiller@redhat.com>
2018-05-18 17:45:08 -0600

    Cleaning up

--- app.rb
+++ app.rb
@@ -1,6 +1,20 @@
 # main app
 require 'sinatra'
 
+set :haml, format: :html5
+
 get '/' do
-  haml :index, format: :html5
+  haml :index
+end
+
+get '/about' do
+  haml :about
+end
+
+get '/cv' do
+  haml :cv
+end
+
+get '/contact' do
+  haml :contact
 end
