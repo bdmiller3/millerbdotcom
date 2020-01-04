b17360fd73ebb03d3efa1ff2561ae2e9b54cc867
Benjamin Miller <bmiller@millerb.com>
2020-01-04 10:22:47 -0500

    Forcing SSL

--- app.rb
+++ app.rb
@@ -3,6 +3,15 @@ require 'sinatra'
 
 set :haml, format: :html5
 
+configure :development, :test do
+  set :host, 'localhost:9999'
+  set :force_ssl, false
+end
+
+configure :production do
+  set :force_ssl, true
+end
+
 get '/' do
   haml :index
 end
