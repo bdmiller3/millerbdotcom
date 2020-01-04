606a87d5ae9358423ab6c9f63e1c7c9fa5fecf53
Benjamin Miller <bmiller@millerb.com>
2020-01-04 11:12:46 -0500

    Blanket redirect to SSL

--- app.rb
+++ app.rb
@@ -2,7 +2,12 @@
 require 'sinatra'
 
 set :haml, format: :html5
-set :force_ssl, true
+
+# Enroce SSL
+# The code is from https://stackoverflow.com/a/26675418/10575903
+before do
+  redirect request.url.sub('http', 'https') unless request.secure?
+end
 
 get '/' do
   haml :index
