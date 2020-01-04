5d9a8ca02d743177e82257a9dde1913d3a3ec470
Benjamin Miller <bmiller@millerb.com>
2020-01-04 11:26:26 -0500

    Redirect to SSL in production only

--- app.rb
+++ app.rb
@@ -6,7 +6,9 @@ set :haml, format: :html5
 # Enroce SSL
 # The code is from https://stackoverflow.com/a/26675418/10575903
 before do
-  redirect request.url.sub('http', 'https') unless request.secure?
+  if settings.production?
+    redirect request.url.sub('http', 'https') unless request.secure?
+  end
 end
 
 get '/' do
